36871bb568081bbdffe77481c7a34df0ed67175d
Domenico159 <domenico.zummo@gmail.com>
2021-05-04 16:03:17 +0200

    add main

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,49 @@
 <template>
-  <div class="container">
       <footer>
-          
+          <div class="container">
+              <div class="box-links">
+                  <ul>
+                      <li>
+                          <h3>Titolo</h3>
+                          <a href=""></a>
+                          <a href=""></a>
+                          <a href=""></a>
+                          <a href=""></a>
+                          <a href=""></a>
+                      </li>
+                       <li>
+                           <h3>Titolo</h3>
+                          <a href=""></a>
+                          <a href=""></a>
+                          <a href=""></a>
+                          <a href=""></a>
+                          <a href=""></a>
+                      </li>
+                       <li>
+                           <h3>Titolo</h3>
+                          <a href=""></a>
+                          <a href=""></a>
+                          <a href=""></a>
+                          <a href=""></a>
+                          <a href=""></a>
+                      </li>
+                       <li>
+                           <h3>Titolo</h3>
+                          <a href=""></a>
+                          <a href=""></a>
+                          <a href=""></a>
+                          <a href=""></a>
+                          <a href=""></a>
+                      </li>
+                  </ul>
+              </div>
+
+              <div class="box-img">
+              <img src="../assets/img/dc-logo-bg.png" alt="">
+             </div>
+
+          </div>
       </footer>
-  </div>
 </template>
 
 <script>
@@ -13,5 +53,37 @@ export default {
 </script>
 
 <style scoped>
+
+footer{
+    background-image: url(../assets/img/footer-bg.jpg);
+    height: 500px;
+    position: relative;
+    overflow: hidden;
+}
+
+footer .container{
+    display: flex;
+    justify-content: space-between;
+}
+
+footer .container .box-links{
+    padding: 30px 10px;
+}
+
+footer .container .box-links ul {
+    display: flex;
+    flex-wrap: wrap;
+    list-style: none;
+}
+
+footer .container .box-links ul li{
+    flex-basis: calc(100% / 3 - 15px);
+    margin: 7px;
+}
+
+footer .container .box-links ul li a,
+footer .container .box-links ul li h3{
+    color: #fff;
+}
 
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,7 +5,28 @@
                   <h2> Content goes here </h2>
               </div>
               <div class="main-bottom">
-
+                  <ul class="container">
+                      <li>
+                          <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                          <a href="">digital comics</a>
+                      </li>
+                      <li>
+                          <img src="../assets/img/buy-comics-merchandise.png" alt="">
+                          <a href="">dc merchandise</a>
+                      </li>
+                      <li>
+                          <img src="../assets/img/buy-comics-subscriptions.png" alt="">
+                          <a href="">subscriptions</a>
+                      </li>
+                      <li>
+                          <img src="../assets/img/buy-comics-shop-locator.png" alt="">
+                          <a href="">comic shop locator</a>
+                      </li>
+                      <li>
+                          <img src="../assets/img/buy-dc-power-visa.svg" alt="">
+                          <a href="">dc power visa</a>
+                      </li>
+                  </ul>
               </div>
       </main>
   </div>
@@ -29,4 +50,27 @@ main{
     display: flex;
     align-items: center;
 }
+
+.main-bottom{
+    background: dodgerblue;
+}
+
+.main-bottom ul {
+    display: flex;
+    list-style: none;
+    padding: 40px 10px;
+}
+.main-bottom ul li{
+    display: flex;
+    align-items: center;
+    flex-basis: calc(100% / 5 - 10px);
+}
+.main-bottom ul li a {
+    color: #fff;
+    margin-left: 30px;
+}
+.main-bottom ul li img{
+    width: 50px;
+}
+
 </style>
