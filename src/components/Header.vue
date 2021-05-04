7d35eee7831b6931e2b59ed7002a6c68683a6584
Domenico159 <domenico.zummo@gmail.com>
2021-05-04 14:57:26 +0200

    Add vue

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+      <header>
+          <h1>Ciao</h1>
+      </header>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Header',
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+      <main>
+          
+      </main>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Main',
+}
+</script>
+
+<style scoped>
+
+</style>
